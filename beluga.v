1bd68bc50d286469b713a5c59c10183c5e6c782e
Andrew Cave <acave1@cs.mcgill.ca>
2011-02-17 14:36:59 -0500

    fsafa

--- beluga.v
+++ beluga.v
@@ -305,12 +305,12 @@ Definition msubst_typ {α} (Δ:mtype_assign α) {β} (Δ':mtype_assign β) θ :=
 
  Inductive is_exval (D G:world) : checked_exp D G -> Prop :=
   | rec_is_val : forall G2 (f:wlink G G2) E, is_exval D G (rec f E).
- 
+ Definition closed_mterm := meta_term empty.
  Inductive env : world -> Set :=
   | e_nil : env empty
   | e_cons : forall G1 G2, env G1 -> (slink G1 G2)*exval -> env G2
  with val : Set :=
-  | v_meta2 : meta_term empty -> val
+  | v_meta2 : closed_mterm -> val
   | v_val2 : forall D G E, is_val D G E -> msubst D empty -> env G -> val
  with exval : Set :=
   | v_val1 : val -> exval
@@ -318,14 +318,14 @@ Definition msubst_typ {α} (Δ:mtype_assign α) {β} (Δ':mtype_assign β) θ :=
  Implicit Arguments v_val2.
  Implicit Arguments e_nil.
  Implicit Arguments e_cons.
-
+ Coercion v_meta2 : closed_mterm >-> val.
  Inductive closure : Set :=
   | meta_term_closure : meta_term empty -> closure
   | comp_term_closure : forall D G, checked_exp D G -> msubst D empty -> env G -> closure.
  Implicit Arguments comp_term_closure.
 
  Definition val_to_closure (v:val) : closure.
- destruct v. constructor 1. exact m.
+ destruct v. constructor 1. auto.
  set E.
  destruct E; try (elimtype False; inversion i; try inversion H; fail); econstructor 2.
  eexact c. eexact m. exact e.
@@ -399,16 +399,16 @@ Definition msubst_typ {α} (Δ:mtype_assign α) {β} (Δ':mtype_assign β) θ :=
          -> case_i I Bs [θ ;; ρ] ⇓ V
          -> case_i I ((br Ck θk Ek)::Bs) [θ ;; ρ] ⇓ V
   | ev_case2 : forall δ θ γ ρ (I:synth_exp δ γ) δi
-            (θk:msubst δ δi) θ' Bs C Ek V Ck,
+            (θk:msubst δ δi) θ' Bs (C:closed_mterm) Ek V Ck,
             (θ ≐ θk // θ')
-         -> I [θ ;; ρ] ⇓ (v_meta2 C)
+         -> I [θ ;; ρ] ⇓ C
          -> (C /≑ ⟦θ'⟧ Ck)
          -> case_i I Bs [θ ;; ρ] ⇓ V
          -> case_i I ((br Ck θk Ek)::Bs) [θ ;; ρ] ⇓ V
   | ev_case3 : forall δ θ γ ρ (I:synth_exp δ γ) δi
-            (θk:msubst δ δi) θ' θ'' Bs C Ek V Ck,
+            (θk:msubst δ δi) θ' θ'' Bs (C:closed_mterm) Ek V Ck,
             (θ ≐ θk // θ')
-         -> I [θ ;; ρ] ⇓ (v_meta2 C)
+         -> I [θ ;; ρ] ⇓ C
          -> (C ≑ ⟦θ'⟧ Ck // θ'')
          -> Ek [ ⟦θ''⟧ θ' ;; ρ ] ⇓ V
          -> case_i I ((br Ck θk Ek)::Bs) [θ ;; ρ] ⇓ V 
@@ -442,6 +442,56 @@ Definition msubst_typ {α} (Δ:mtype_assign α) {β} (Δ':mtype_assign β) θ :=
                     ⟦theta_weaken θ' X';  m_var X' // X ⟧ T1 =
                     ⟦theta_weaken θ X';  m_var X' // X0 ⟧ T ->
                     ⟦θ';  m // X ⟧ T1 = ⟦θ;  m // X0 ⟧ T.
+   Admitted.
+  Set Implicit Arguments.
+  Section hop1.
+   Variables (δ δ' δ'':world)
+   (θ:msubst δ δ') (θ':msubst δ δ'') (θ'':msubst δ'' δ')
+   (Δ:mtype_assign δ) (Δ':mtype_assign δ') (Δ'':mtype_assign δ'').
+   Theorem hop1a :
+       Δ' ⊩ θ ∷ Δ
+    -> Δ'' ⊩ θ' ∷ Δ
+    -> θ ≐ θ' // θ''
+    -> Δ' ⊩ θ'' ∷ Δ''.
+   Admitted.
+   Theorem hop1b :
+       Δ' ⊩ θ ∷ Δ
+    -> Δ'' ⊩ θ' ∷ Δ
+    -> θ ≐ θ' // θ''
+    -> θ = ⟦θ''⟧ θ'.
+   Admitted.
+  End hop1.
+ 
+  Section hop2.
+   Variables (δ δ':world)
+   (θ:msubst δ δ')
+   (Δ:mtype_assign δ) (Δ':mtype_assign δ')
+   (T:mtype δ') (C:meta_term δ')
+   (U:mtype δ)  (D:meta_term δ).
+   Theorem hop2a :
+       Δ' ⊨ C ∷ T
+    -> Δ  ⊨ D ∷ U
+    -> C ≑ D // θ
+    -> Δ' ⊩ θ ∷ Δ.
+   Admitted.
+   Theorem hop2b :
+       Δ' ⊨ C ∷ T
+    -> Δ  ⊨ D ∷ U
+    -> C ≑ D // θ
+    -> C = ⟦θ⟧ D.
+   Admitted.
+  End hop2.
+
+   Lemma subst_assoc {δ δ' δ''} (T:mtype δ)
+    (θ:msubst δ δ') (θ':msubst δ' δ'') :
+    ⟦⟦θ'⟧θ⟧ T = ⟦θ'⟧(⟦θ⟧T).
+   Admitted.
+   Lemma subst_assoc2 {δ δ' δ'' γ} (Γ:tp_assign δ γ)
+    (θ:msubst δ δ') (θ':msubst δ' δ'') :
+    app_msubst_tp_assign (⟦θ'⟧θ) Γ =
+     app_msubst_tp_assign θ' (app_msubst_tp_assign θ Γ).
+   Admitted.
+   Lemma subst_id {δ} (θ:msubst δ empty) : ⟦·⟧ θ = θ.
    Admitted.
 
    Theorem subj_red L V : L ⇓ V -> forall T, L ∷∷ T -> V ∷∷ T.
@@ -555,7 +605,44 @@ Definition msubst_typ {α} (Δ:mtype_assign α) {β} (Δ':mtype_assign β) θ :=
    constructor 2. auto.
 
    (* case statement 3 *)
+   inversion H12. subst.
+   assert (C ∷∷ a θ U).
+   eapply IHeval1.
+   econstructor.
+   eexact H10.
+   eexact H11.
+   constructor. auto.
+   inversion H4. subst.
    
+   assert (@br_tp _ _ Δ Γ (br Ck θk Ek) (arr U T0)).
+   eapply H8.
+   constructor.
+   reflexivity.
+   inversion H5. subst. simpl_existTs. subst.
+   pose proof (hop1a H10 H20 H).
+   assert (· ⊩ θ'' ∷ ·).
+   eapply hop2a.
+   Focus 3. eexact H1.
+   eexact H9.
+   eapply subst_lemma.   
+   eexact H13.
+   eexact H16.
+   pose proof (hop1b H10 H20 H). 
+   subst.
+   inversion H14. simpl_existTs. subst. (* We diverge here *)
+   eapply IHeval2.
+   simpl.
+   rewrite subst_assoc.
+   rewrite subst_id.
+   change (Ek [θ';; ρ] ∷∷ a θ' (app_msubst_t θk T)).
+   econstructor.
+   eexact H13.   
+   pose proof (subst_assoc2 Γ θk θ').
+   unfold app_subst in H11 at 1.
+   unfold tp_assign_substitutable in H11.
+   rewrite H15 in H11.
+   eexact H11.
+   auto.
    Qed.
    
 End foo.
