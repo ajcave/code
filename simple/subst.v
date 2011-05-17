eba96c7598f663d4808c26a44f311839f649a126
Andrew Cave <acave1@cs.mcgill.ca>
2011-05-17 14:27:47 -0400

    Added case to substition based semantics proof

--- simple/subst.v
+++ simple/subst.v
@@ -5,37 +5,35 @@ Set Implicit Arguments.
 
 Section rename_sect.
 Reserved Notation "[ θ ] T " (at level 90).
-
+Definition rn_cons {α β γ δ} (θ:name α -> name β) (y:β↪γ) (x:α↪δ) := ((import y) ○ θ,,(x,y)).
+Notation "θ × ( y // x )" := (rn_cons θ y x) (at level 90).
 Fixpoint rename {α β} (θ:name α -> name β) (M:exp α) : exp β := match M with
 | var n => var (θ n)
 | tt => tt
 | app M N => app ([θ] M) ([θ] N)
-| lam _ x T M => 
-  let (_,x') := next β in
-  lam x' T ([(import x') ○ θ,,(x,x')] M)
+| lam _ y T M => 
+  let (_,z) := next β in
+  lam z T ([θ × (z // y)] M)
 | inl T M => inl T ([θ] M)
 | inr T M => inr T ([θ] M)
+| case M _ x N1 _ y N2 =>
+  let (_,z) := next β in
+  case ([θ] M)
+         z ([θ × (z // x)] N1)
+         z ([θ × (z // y)] N2)
 end
 where "[ θ ] M" := (rename θ M).
-End rename_sect.
 
-Definition import_exp {α β} (x:α↪β) : exp α -> exp β :=
- rename (import x).
-
-Reserved Notation "[ θ ] T" (at level 90).
-
-Fixpoint app_subst {α β} (θ:name α -> exp β) (M:exp α) : exp β :=
-match M with
-| var n => θ n
-| tt => tt
-| app M N => app ([θ] M) ([θ] N)
-| lam _ x T M => 
-  let (β',x') := next β in
-  lam x' T ([(import_exp x') ○ θ,,(x,x')] M)
-| inl T M => inl T ([θ] M)
-| inr T M => inr T ([θ] M)
-end
-where "[ θ ] M" := (app_subst θ M).
+Lemma rn_cons_tp {γ γ0 γ1 γ'} (Γ:tp_ctx γ) (Γ0:tp_ctx  γ0) (θ:name γ -> name γ0) (y:γ0↪γ1) T (x:γ↪γ') :
+ (forall z, Γ0 (θ z) = Γ z) ->
+ forall z, (Γ0,, (y, T)) ((θ × (y // x)) z) = (Γ,,(x,T)) z.
+intros.
+unfold rn_cons.
+unfold compose at 2 4.
+case (export x z); simpl; intros; unfold compose.
+erewrite export_import_inv. simpl. eauto.
+erewrite export_self. reflexivity.
+Qed.
 
 Lemma rename_lemma : forall {δ} M T (Δ:tp_ctx δ),
   Δ ⊢ M ⇐ T ->
@@ -46,16 +44,56 @@ induction 1; simpl; intros; subst; eauto.
 econstructor.
 set (next_link γ0).
 eapply IHof.
-intros.
-unfold compose at 2 4.
-case (export x x0); simpl; intros.
-unfold compose. erewrite export_import_inv.
-simpl. eauto.
-unfold compose. erewrite export_self.
-reflexivity.
+eapply rn_cons_tp; eauto.
+econstructor; eauto.
+eapply IHof2.
+eapply rn_cons_tp; eauto.
+eapply IHof3.
+eapply rn_cons_tp; eauto.
 Qed.
+End rename_sect.
+
+Definition import_exp {α β} (x:α↪β) : exp α -> exp β :=
+ rename (import x).
+
+Reserved Notation "[ θ ] T" (at level 90).
+Definition subst_cons {α β γ δ} (θ:name α -> exp β) (y:β↪γ) (x:α↪δ) := ((import_exp y) ○ θ,,(x,y)).
+Notation "θ × ( y // x )" := (subst_cons θ y x) (at level 90).
+Fixpoint app_subst {α β} (θ:name α -> exp β) (M:exp α) : exp β :=
+match M with
+| var n => θ n
+| tt => tt
+| app M N => app ([θ] M) ([θ] N)
+| lam _ x T M => 
+  let (_,y) := next β in
+  lam y T ([θ × (y // x)] M)
+| inl T M => inl T ([θ] M)
+| inr T M => inr T ([θ] M)
+| case M _ x N1 _ y N2 =>
+  let (_,z) := next β in
+  case ([θ] M)
+       z ([θ × (z // x)] N1)
+       z ([θ × (z // y)] N2)      
+end
+where "[ θ ] M" := (app_subst θ M).
+
 
 Notation "Γ ⊩ θ ⇐ Δ" := (forall x, Γ ⊢ θ x ⇐ Δ x) (at level 90).
+
+Lemma subst_cons_tp  {γ γ0 γ1 γ'} (Γ:tp_ctx γ) (Γ0:tp_ctx  γ0) (θ:name γ -> exp γ0) (y:γ0↪γ1) T (x:γ↪γ') :
+ Γ0 ⊩ θ ⇐ Γ ->
+(Γ0,, (y, T)) ⊩ θ × (y // x) ⇐ (Γ,,(x,T)).
+intros.
+unfold subst_cons.
+unfold compose at 2 4.
+case (export x x0); intros; simpl.
+eapply rename_lemma; eauto.
+intros. unfold compose. erewrite export_import_inv.
+reflexivity.
+econstructor. unfold compose.
+erewrite export_self.
+reflexivity.
+Qed.
 
 Lemma subst_lemma : forall {δ} M T (Δ:tp_ctx δ),
    Δ ⊢ M ⇐ T   ->
@@ -66,15 +104,12 @@ induction 1; simpl; intros; subst; eauto.
 set (next_link γ0).
 econstructor.
 eapply IHof.
-intro.
-unfold compose at 2 4.
-case (export x x0); intros; simpl.
-eapply rename_lemma; eauto.
-intros. unfold compose. erewrite export_import_inv.
-reflexivity.
-econstructor.
-unfold compose. erewrite export_self.
-reflexivity.
+eapply subst_cons_tp; eauto.
+econstructor; eauto.
+eapply IHof2.
+eapply subst_cons_tp; eauto.
+eapply IHof3.
+eapply subst_cons_tp; eauto.
 Qed.
 
 Definition single_subst {γ γ'} (x:γ↪γ') (M:exp γ') (N:exp γ) := [@var γ,,(x,N)] M. 
@@ -103,14 +138,27 @@ Inductive eval : exp ∅ -> exp ∅ -> Prop :=
 | eval_app : forall M {γ} T (x:∅↪γ) (M':exp γ) N V1 V2 ,
      M ⇓ (lam x T M') ->
      N ⇓ V1 ->
-     (single_subst x M' V1) ⇓ V2 ->
+     (single_subst x M' V1) ⇓ V2
+   (*==========================*) ->
      (app M N) ⇓ V2 
 | eval_inl : forall M T V,
-     M ⇓ V ->
+     M ⇓ V 
+   (*==========================*) ->
      (inl T M) ⇓ (inl T V)
 | eval_inr : forall M T V,
-     M ⇓ V ->
+     M ⇓ V
+   (*==========================*) ->
      (inr T M) ⇓ (inr T V)
+| eval_case_l : forall M {α β} {x:∅↪α} N1 {y:∅↪β} N2 T U V,
+     M ⇓ (inl T U) ->
+     (single_subst x N1 U) ⇓ V
+   (*==========================*) ->
+     (case M x N1 y N2) ⇓ V
+| eval_case_r : forall M {α β} {x:∅↪α} N1 {y:∅↪β} N2 T U V,
+     M ⇓ (inr T U) ->
+     (single_subst y N2 U) ⇓ V
+   (*==========================*) ->
+     (case M x N1 y N2) ⇓ V
 where "M ⇓ V" := (eval M V).
 
 Hint Constructors eval.
@@ -126,6 +174,16 @@ apply IHeval2.
 assumption.
 nice_inversion H0. eauto.
 nice_inversion H0. eauto.
+nice_inversion H1.
+eapply IHeval2.
+eapply single_subst_lemma; eauto.
+apply IHeval1 in H10.
+nice_inversion H10. auto.
+nice_inversion H1.
+eapply IHeval2.
+eapply single_subst_lemma; eauto.
+apply IHeval1 in H10.
+nice_inversion H10. auto.
 Qed.
 
 Reserved Notation "M ⇑" (at level 90).
@@ -148,6 +206,20 @@ CoInductive diverge : exp ∅ -> Prop :=
 | inr_div : forall M T,
      M ⇑ ->
      (inr T M) ⇑
+| case_div : forall M {α β} {x:∅↪α} N1 {y:∅↪β} N2,
+     M ⇑
+   (*==========================*) ->
+     (case M x N1 y N2) ⇑
+| case_l_div : forall M {α β} {x:∅↪α} N1 {y:∅↪β} N2 T U,
+     M ⇓ (inl T U) ->
+     (single_subst x N1 U) ⇑
+   (*==========================*) ->
+     (case M x N1 y N2) ⇑
+| case_r_div : forall M {α β} {x:∅↪α} N1 {y:∅↪β} N2 T U,
+     M ⇓ (inr T U) ->
+     (single_subst y N2 U) ⇑
+   (*==========================*) ->
+     (case M x N1 y N2) ⇑
 where "M ⇑" := (diverge M).
 
 Hint Constructors val.
@@ -163,6 +235,7 @@ Lemma canonical_forms {V T} : val V -> · ⊢ V ⇐ T -> canonical V T.
 intros. nice_inversion H0; eauto.
 destruct (empty_is_empty x).
 nice_inversion H.
+inversion H.
 Qed.
 
 Lemma eval_to_val {M V} : M ⇓ V -> val V.
@@ -207,6 +280,30 @@ econstructor.
 destruct (classical (exists V, M0 ⇓ V)).
 destruct H2.
 destruct (H0 (inr T0 x)); eauto.
+eapply progress; eauto.
+destruct (classical (exists V, M0 ⇓ V)).
+destruct H4 as [V Hy].
+pose proof (subject_reduction Hy H1).
+pose proof (eval_to_val Hy).
+pose proof (canonical_forms H5 H4).
+nice_inversion H6.
+destruct (classical (exists V, (single_subst x N1 M) ⇓ V)).
+destruct H7 as [V1 Hy0].
+edestruct H0.
+eapply eval_case_l; eauto.
+eapply case_l_div; eauto.
+eapply progress; eauto.
+nice_inversion H4.
+eapply single_subst_lemma; eauto.
+destruct (classical (exists V, (single_subst y N2 M) ⇓ V)).
+destruct H7 as [V1 Hy0].
+edestruct H0.
+eapply eval_case_r; eauto.
+eapply case_r_div; eauto.
+eapply progress; eauto.
+nice_inversion H4.
+eapply single_subst_lemma; eauto.
+eapply case_div.
 eapply progress; eauto.
 Qed.
 
