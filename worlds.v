a8ae567666e73952240d873003c39c8c5d3a850d
Andrew Cave <acave1@cs.mcgill.ca>
2011-05-17 11:26:29 -0400

    Removed name' junk

--- worlds.v
+++ worlds.v
@@ -12,33 +12,20 @@ Implicit Arguments s_nil [A Rel a].
 Parameter world : Set.
 Parameter empty : world.
 Notation "∅" := empty.
-Parameter name' : world -> world -> Set.
-Definition name := name' ∅.
-Axiom self_empty : forall α, name' α α -> False.
-Lemma empty_is_empty : name ∅ -> False.
-eapply self_empty.
-Qed.
+Parameter name : world -> Set.
+Axiom empty_is_empty : name ∅ -> False.
 Parameter link : world -> world -> Set.
 Notation "α ↪ β" := (link α β) (at level 90).
 
 Definition link_star := star link.
 Notation "α ↪* β" := (link_star α β) (at level 90).
-Axiom weaken' : forall {α β γ}, α↪*β -> β↪γ -> name' α γ.
 Parameter weaken : forall {α β}, α↪β -> name β.
 Coercion weaken : link >-> name.
-Axiom import' : forall {α β γ}, β↪γ -> name' α β -> name' α γ.
-Corollary import : forall {α β}, α↪β -> name α -> name β.
-eapply (@import' ∅).
-Qed.
+Axiom import : forall {α β}, α↪β -> name α -> name β.
 Parameter next' : forall a, {b:world & a↪b}.
 Definition next a : {b:world & a↪b} := existT _ (projT1 (next' a)) (projT2 (next' a)).
 
-Axiom export' : forall {α β γ} (y:β↪γ) (n:name' α γ),
- name' α β + unit.
-
-Lemma export : forall {α β} (y:α↪β) (n:name β), name α + unit.
-eapply (@export' ∅).
-Qed.
+Axiom export : forall {α β} (y:α↪β) (n:name β), name α + unit.
 
 Axiom export_self : forall {α β} (y:α↪β), export y y = inr _ tt.
 
