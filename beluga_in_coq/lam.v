75af01f631f35c0e8233398a41df619d5a1053e3
Andrew Cave <acave1@cs.mcgill.ca>
2011-05-28 08:00:53 -0400

    Added eta contraction (minus strengthening view)

--- beluga_in_coq/lam.v
+++ beluga_in_coq/lam.v
@@ -1,20 +1,49 @@
 Set Implicit Arguments.
 Require Import Coq.Program.Equality.
 Require Import Coq.Logic.FunctionalExtensionality.
+Require Import List.
 Parameter world : Set.
 Parameter link : world -> world -> Set.
 Parameter name : world -> Set.
 Notation "α ↪ β" := (link α β) (at level 90).
 Parameter cast : forall {α β}, α↪β -> name β.
 Coercion cast  : link >-> name.
+Parameter empty : world.
+Notation "∅" := empty.
 Parameter s_w : world -> world.
 Parameter s_l : forall α, α↪(s_w α). 
 Parameter import : forall {α β}, α↪β -> name α -> name β.
 Axiom export : forall {α β} (y:α↪β) (n:name β), name α + unit.
 Axiom export_self : forall {α β} (y:α↪β), export y y = inr _ tt.
 Axiom export_import_inv : forall {α β} (y:α↪β) (n:name α), export y (import y n) = inl _ n.
-Axiom destruct1 : forall α β γ (x:α↪β) (y:α↪γ), β = γ.
-Axiom destruct : forall α β γ (x:α↪β) (y:α↪γ), x ~= y.
+Axiom export_inr : forall {α β} (y:α↪β) x v, inr _ v = export y x -> x = y.  
+Axiom export_inl : forall {α β} (y:α↪β) x v, inl _ v = export y x -> x = import y v.
+(* Axiom destruct1 : forall α β γ (x:α↪β) (y:α↪γ), β = γ.
+Axiom destruct : forall α β γ (x:α↪β) (y:α↪γ), x ~= y. *)
+Axiom empty_is_empty : forall {C:Set}, name ∅ -> C.
+
+Inductive star {A} (R:A -> A -> Set) (a:A) : A -> Type :=
+| s_nil : star R a a
+| s_cons : forall b c, star R a b -> R b c -> star R a c.
+Notation "a ↪* b" := (star link a b) (at level 90).
+
+Axiom empty_is_initial : forall α, ∅↪*α.
+
+Fixpoint export_star {α β} (l:α↪*β) : name β -> name α + unit :=
+match l in star _ _ b return name b -> name α + unit with
+| s_nil => fun x => inl _ x
+| s_cons _ _ ys y => fun x =>
+  match (export y x) with
+   | inl x' => export_star ys x'
+   | inr _ => inr _ tt
+  end
+end.
+
+Definition list_from_maybe {A} (x:A + unit) : list A :=
+match x with
+| inl a => a::nil
+| inr _ => nil
+end.
 
 Definition compose {A B C} (f:A -> B) (g:B -> C) (a:A) := g (f a).
 Definition maybe {A C} (f:A -> C) (c:C) (x:A + unit) : C := match x with | inl a => f a | inr _ => c end.  
@@ -56,7 +85,7 @@ Inductive view ψ : exp ψ -> Set :=
             view (app ([σ1]M1) ([σ2]M2)).
 
 Definition id {A} := fun (x:A) => x.
-
+Notation "…" := id.
 
 Theorem foo : forall (ψ φ φ0 φ0' χ χ': world) (l : ψ ↪ φ) 
                            (σ1 : ψ [φ0]) (σ2 : φ0 [χ]) 
@@ -119,14 +148,103 @@ exact IHM2.
 Qed.
 
 Definition exchange {ψ φ χ} (x:ψ↪φ) (y:φ↪χ) := 
- ((import y ○ import x ○ id,, (x,y),, (y,import y x))).
+ ((import y ○ import x ○ …,, (x,y),, (y,import y x))).
 (* TODO: Is this a use case for name α β and general × ? *)
 
 Fixpoint cnt {ψ'} {m:exp ψ'} (M:sview m) {ψ} (x:ψ↪ψ') : nat :=
 match M with
 | var_s y => if (export x y) then 0 else 1
 | lam_s _ y _ N => cnt (N _ (exchange x y)) y (* Swap x for y and then count the ys *)
-| app_s _ M1 _ M2 => (cnt (M1 _ id) x) + (cnt (M2 _ id) x)
+| app_s _ M1 _ M2 => (cnt (M1 _ …) x) + (cnt (M2 _ …) x)
 end.
 (* We could use a 'different' world for N, and name the new
    variables x and y to make it look sane... *) 
+
+
+Fixpoint fv {φ} (M:exp φ) {ψ} (Γ:ψ↪*φ) : list (name ψ) :=
+match M with
+| var x => list_from_maybe (export_star Γ x)
+| lam _ x R => fv R (s_cons _ Γ x)
+| app M1 M2 => (fv M1 Γ) ++ (fv M2 Γ)
+end. 
+
+Inductive subst_range {ψ ψ' φ} (y:ψ↪ψ') : forall (σ:φ[ψ']), Set :=
+| subst_range_no : forall (σ':φ[ψ]), subst_range y (import y ○ σ')
+| subst_range_yes  : forall (σ:φ[ψ']) x, σ x = y -> subst_range y σ.
+
+Lemma subst_congr {ψ ψ' φ χ} (σ1:ψ[φ]) (σ2:φ[χ]) (y:ψ↪ψ') x : (σ2 ○ (σ1,,(y,x))) = (σ2 ○ σ1,,(y,σ2 x)). 
+eapply functional_extensionality_dep; intros.
+unfold compose at 1 2 3.
+destruct (export y x0); reflexivity.
+Qed.
+
+Lemma decompose_subst {ψ ψ' φ} (y:ψ↪ψ') (σ:ψ'[φ]) : σ = ((σ ○ import y),,(y,σ y)).
+eapply functional_extensionality_dep. intros.
+unfold compose at 1.
+remember (export y x).
+destruct s; simpl.
+pose proof (export_inl Heqs). subst.
+reflexivity.
+pose proof (export_inr Heqs). subst.
+reflexivity.
+Qed.
+
+Lemma is_in_range {ψ ψ' φ} (y:ψ↪ψ') (σ:φ[ψ']) : subst_range y σ.
+pose proof (empty_is_initial φ) as l.
+induction l.
+assert (σ = ((import y) ○ empty_is_empty)).
+eapply functional_extensionality_dep; intros.
+destruct (@empty_is_empty False x).
+subst.
+econstructor; eauto.
+specialize (IHl (σ ○ (import r))).
+inversion IHl.
+
+remember (export y (σ r)).
+destruct s.
+
+pose proof (decompose_subst r σ).
+pose proof (export_inl Heqs).
+rewrite H1 in H.
+rewrite <- H0 in H.
+erewrite <- subst_congr in H.
+rewrite H.
+econstructor 1; eauto.
+
+pose proof (export_inr Heqs).
+econstructor 2; eauto.
+
+econstructor 2.
+unfold compose in H.
+eexact H.
+Qed.
+
+Notation "x ≠ y" := (export x y) (at level 90).
+
+(* TODO: Define the strengthening view! *)
+
+(* Note that there is some extra "flexibility" in this 
+   definition corresponding to "how much strengthening to attempt".
+   We can do none (id_view), all (str_view) or some combination.
+   It's not clear to me if there are applications that might benefit
+   from exploiting this. Maybe some terms are really big or far away or
+   expensive in some sense, or "opaque", so you don't want to check them
+   for occurrences. *)
+(* Perform eta contractions recursively *)
+Fixpoint eta {ψ} {m:exp ψ} (M:view m) : exp ψ :=
+match M with
+| var_v x => var x
+| lam_v _ _ x _ N σ =>
+ match (N _ σ) with 
+ | app_v _ m1 M1 σ1 _ (var y) M2 σ2 =>
+    if x ≠ (σ2 y) then
+       lam x (eta (N _ σ))
+    else
+       match (is_in_range x σ1) with
+        | subst_range_no σ1' => eta (M1 _ σ1')
+        | _ => lam x (eta (N _ σ))
+       end
+ | _ => lam x (eta (N _ σ))
+ end
+| app_v _ _ M1 σ1 _ _ M2 σ2 => app (eta (M1 _ σ1)) (eta (M2 _ σ2))
+end.
