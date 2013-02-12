c0e3ef38bbfa906d80de6f67d5260a602ba38099
Andrew Cave <ajcave@coqbox.(none)>
2013-02-11 20:08:29 -0500

    blah

---
+++ mu-calculus/strong_norm_coq/sn.v
@@ -0,0 +1,87 @@
+Set Implicit Arguments.
+Inductive ctx (A : Type) : Type :=
+ | nil : ctx A
+ | snoc : ctx A -> A -> ctx A.
+Implicit Arguments nil [A].
+
+Inductive var (A : Type) : ctx A -> A -> Type :=
+ | top : forall G T, var (snoc G T) T
+ | pop : forall G T S, var G T -> var (snoc G S) T.
+Implicit Arguments top [A G T].
+Implicit Arguments pop [A G T S].
+
+Open Scope type_scope.
+Fixpoint gsub (A : Type) (G : ctx A) (F : A -> Type) : Type :=
+match G with
+| nil => unit
+| snoc G' T => (gsub G' F) * (F T)
+end.
+
+Fixpoint glookup (A : Type) (G : ctx A) (F : A -> Type) T (x : var G T) : gsub G F -> F T :=
+match x in var G T return gsub G F -> F T with
+| top G' T' => fun s => snd s
+| pop G' T' S' y => fun s => glookup F y (fst s)
+end.
+
+Fixpoint gmap (A : Type) (G : ctx A) (F1 F2 : A -> Type) (f : forall T, F1 T -> F2 T) : gsub G F1 -> gsub G F2 :=
+match G return gsub G F1 -> gsub G F2 with
+| nil => fun s => s
+| snoc G' T => fun s => pair (gmap G' F1 F2 f (fst s)) (f T (snd s))
+end.
+
+Definition vsub (A : Type) (G G' : ctx A) := gsub G (var G').
+
+Definition wknvsub (A : Type) (G G' : ctx A) T (s : vsub G G') : vsub G (snoc G' T) :=
+gmap G (var G') (var (snoc G' T)) (fun T x => pop x) s.
+
+Definition extvsub (A : Type) (G G' : ctx A) T (s : vsub G G') : vsub (snoc G T) (snoc G' T) :=
+pair (wknvsub G G' T s) top.
+
+Fixpoint idvsub (A : Type) (G : ctx A) : vsub G G :=
+match G return vsub G G with
+| nil => tt
+| snoc G' T => extvsub G' G' T (idvsub G')
+end.
+
+Definition weakening_vsub (A : Type) (G : ctx A) T : vsub G (snoc G T) :=
+wknvsub G G T (idvsub G).
+
+Inductive sort : Type :=
+ | type : sort.
+
+Inductive functor (D : ctx sort) : Set :=
+ | fv : var D type -> functor D
+ | arrow : functor nil -> functor D -> functor D
+ | times : functor D -> functor D -> functor D
+ | plus : functor D -> functor D -> functor D
+ | mu : functor (snoc D type) -> functor D
+ | nu : functor (snoc D type) -> functor D.
+
+Fixpoint app_vsub D D' (F : functor D) (s : vsub D D') : functor D' :=
+match F with
+| fv x => fv (glookup (var _) x s)
+| arrow A F' => arrow A (app_vsub D' F' s)
+| times F G => times (app_vsub D' F s) (app_vsub D' G s)
+| plus F G => plus (app_vsub D' F s) (app_vsub D' G s)
+| mu F => mu (app_vsub _ F (extvsub D D' type s))
+| nu F => nu (app_vsub _ F (extvsub D D' type s))
+end.
+
+Definition fsub (D D' : ctx sort) := gsub D (fun _ => functor D').
+
+
+
+Definition tp := functor nil.
+
+Inductive tm (G : ctx tp) : tp -> Set :=
+ | v : forall T, var G T -> tm G T
+ | lam : forall T S, tm (snoc G T) S -> tm G (arrow T S)
+ | app : forall T S, tm G (arrow T S) -> tm G T -> tm G S
+ | pair : forall T S, tm G T -> tm G S -> tm G (times T S)
+ | fst : forall T S, tm G (times T S) -> tm G T
+ | snd : forall T S, tm G (times T S) -> tm G S
+ | inl : forall S T, tm G T -> tm G (plus T S)
+ | inr : forall T S, tm G S -> tm G (plus T S)
+ | case : forall T S C, tm G (plus T S) -> tm (snoc G T) C -> tm (snoc G S) C -> tm G C
+ | 
+.
