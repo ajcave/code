d628012a7dbccaca2311614371eae0bfd5a38c59
Andrew Cave <ajcave@coqbox.(none)>
2013-02-11 21:05:17 -0500

    blah

--- mu-calculus/strong_norm_coq/sn.v
+++ mu-calculus/strong_norm_coq/sn.v
@@ -36,15 +36,17 @@ gmap G (var G') (var (snoc G' T)) (fun T x => pop x) s.
 
 Definition extvsub (A : Type) (G G' : ctx A) T (s : vsub G G') : vsub (snoc G T) (snoc G' T) :=
 pair (wknvsub G G' T s) top.
+Implicit Arguments extvsub [ A G G' T ].
 
 Fixpoint idvsub (A : Type) (G : ctx A) : vsub G G :=
 match G return vsub G G with
 | nil => tt
-| snoc G' T => extvsub G' G' T (idvsub G')
+| snoc G' T => extvsub (idvsub G')
 end.
+Implicit Arguments idvsub [A G].
 
 Definition weakening_vsub (A : Type) (G : ctx A) T : vsub G (snoc G T) :=
-wknvsub G G T (idvsub G).
+wknvsub G G T idvsub.
 
 Inductive sort : Type :=
  | type : sort.
@@ -63,8 +65,8 @@ match F with
 | arrow A F' => arrow A (app_vsub D' F' s)
 | times F G => times (app_vsub D' F s) (app_vsub D' G s)
 | plus F G => plus (app_vsub D' F s) (app_vsub D' G s)
-| mu F => mu (app_vsub _ F (extvsub D D' type s))
-| nu F => nu (app_vsub _ F (extvsub D D' type s))
+| mu F => mu (app_vsub _ F (extvsub s))
+| nu F => nu (app_vsub _ F (extvsub s))
 end.
 
 Definition fsub (D D' : ctx sort) := gsub D (fun _ => functor D').
@@ -99,41 +101,98 @@ app_fsub D F (single_fsub T G).
 Definition tp := functor nil.
 
 Inductive tm (G : ctx tp) : tp -> Set :=
- | v : forall T, var G T -> tm G T
- | lam : forall T S, tm (snoc G T) S -> tm G (arrow T S)
- | app : forall T S, tm G (arrow T S) -> tm G T -> tm G S
- | pair : forall T S, tm G T -> tm G S -> tm G (times T S)
- | fst : forall T S, tm G (times T S) -> tm G T
- | snd : forall T S, tm G (times T S) -> tm G S
- | inl : forall S T, tm G T -> tm G (plus T S)
- | inr : forall T S, tm G S -> tm G (plus T S)
- | case : forall T S C, tm G (plus T S) -> tm (snoc G T) C -> tm (snoc G S) C -> tm G C
- | inj : forall F, tm G (app_fsub1 F (mu F)) -> tm G (mu F)
- | rec : forall F C, tm G (mu F) -> tm (snoc nil (app_fsub1 F C)) C -> tm G C
- | out : forall F, tm G (nu F) -> tm G (app_fsub1 F (nu F))
- | corec : forall F C, tm G C -> tm (snoc nil C) (app_fsub1 F C) -> tm G (nu F)
+ | tv : forall T, var G T -> tm G T
+ | tlam : forall T S, tm (snoc G T) S -> tm G (arrow T S)
+ | tapp : forall T S, tm G (arrow T S) -> tm G T -> tm G S
+ | tpair : forall T S, tm G T -> tm G S -> tm G (times T S)
+ | tfst : forall T S, tm G (times T S) -> tm G T
+ | tsnd : forall T S, tm G (times T S) -> tm G S
+ | tinl : forall S T, tm G T -> tm G (plus T S)
+ | tinr : forall T S, tm G S -> tm G (plus T S)
+ | tcase : forall T S C, tm G (plus T S) -> tm (snoc G T) C -> tm (snoc G S) C -> tm G C
+ | tinj : forall F, tm G (app_fsub1 F (mu F)) -> tm G (mu F)
+ | trec : forall F C, tm G (mu F) -> tm (snoc nil (app_fsub1 F C)) C -> tm G C
+ | tout : forall F, tm G (nu F) -> tm G (app_fsub1 F (nu F))
+ | tcorec : forall F C, tm G C -> tm (snoc nil C) (app_fsub1 F C) -> tm G (nu F)
 .
+
+Fixpoint app_vsub_tm G G' T (t : tm G T) (s : vsub G G') : tm G' T :=
+match t with
+| tv T' x => tv (glookup (var G') x s)
+| tlam T1 T2 t' => tlam (app_vsub_tm _ t' (extvsub s))
+| tapp T1 T2 t1 t2 => tapp (app_vsub_tm _ t1 s) (app_vsub_tm _ t2 s)
+| tpair T1 T2 t1 t2 => tpair (app_vsub_tm _ t1 s) (app_vsub_tm _ t2 s)
+| tfst T1 T2 t1 => tfst (app_vsub_tm _ t1 s)
+| tsnd T1 T2 t2 => tsnd (app_vsub_tm _ t2 s)
+| tinl T1 T2 t1 => tinl T1 (app_vsub_tm _ t1 s)
+| tinr T1 T2 t1 => tinr T1 (app_vsub_tm _ t1 s)
+| tcase T1 T2 C t1 t2 t3 => tcase (app_vsub_tm _ t1 s) (app_vsub_tm _ t2 (extvsub s)) (app_vsub_tm _ t3 (extvsub s))
+| tinj F t1 => tinj F (app_vsub_tm _ t1 s)
+| trec F C t1 t2 => trec (app_vsub_tm _ t1 s) t2
+| tout F t1 => tout (app_vsub_tm _ t1 s)
+| tcorec F C t1 t2 => tcorec F (app_vsub_tm _ t1 s) t2
+end.
+
+Definition tsub (D D' : ctx tp) := gsub D (tm D').
+
+Definition wkntsub (G G' : ctx tp) T (s : tsub G G') : tsub G (snoc G' T) :=
+gmap G (tm G') (tm (snoc G' T)) (fun _ t => app_vsub_tm _ t (weakening_vsub G' T)) s.
+
+Definition exttsub (G G' : ctx tp) T (s : tsub G G') : tsub (snoc G T) (snoc G' T) :=
+pair (wkntsub G G' T s) (tv (@top _ G' T)).
+Implicit Arguments exttsub [G G' T].
+
+Fixpoint idtsub (G : ctx tp) : tsub G G :=
+match G return tsub G G with
+| nil => tt
+| snoc G' T => exttsub (idtsub G')
+end.
+Implicit Arguments idtsub [G].
+
+Fixpoint app_tsub D D' T (t : tm D T) (s : tsub D D') : tm D' T :=
+match t with
+| tv T' x => glookup _ x s
+| tlam T1 T2 t' => tlam (app_tsub _ t' (exttsub s))
+| tapp T1 T2 t1 t2 => tapp (app_tsub _ t1 s) (app_tsub _ t2 s)
+| tpair T1 T2 t1 t2 => tpair (app_tsub _ t1 s) (app_tsub _ t2 s)
+| tfst T1 T2 t1 => tfst (app_tsub _ t1 s)
+| tsnd T1 T2 t2 => tsnd (app_tsub _ t2 s)
+| tinl T1 T2 t1 => tinl T1 (app_tsub _ t1 s)
+| tinr T1 T2 t1 => tinr T1 (app_tsub _ t1 s)
+| tcase T1 T2 C t1 t2 t3 => tcase (app_tsub _ t1 s) (app_tsub _ t2 (exttsub s)) (app_tsub _ t3 (exttsub s))
+| tinj F t1 => tinj F (app_tsub _ t1 s)
+| trec F C t1 t2 => trec (app_tsub _ t1 s) t2
+| tout F t1 => tout (app_tsub _ t1 s)
+| tcorec F C t1 t2 => tcorec F (app_tsub _ t1 s) t2
+end.
+
+Definition single_tsub D T (t : tm D T)  : tsub (snoc D T) D := pair idtsub t.
+
+Definition app_tsub1 D T S (t : tm (snoc D T) S) (t2 : tm D T) : tm D S :=
+app_tsub _ t (single_tsub t2).
+Implicit Arguments app_tsub1 [ D T S ].
 
 (* Gotta define map... *)
 
 Inductive step (G : ctx tp) : forall (T : tp), tm G T -> tm G T -> Prop :=
-| step_lam : forall T S (t1 t2 : tm (snoc G T) S), @step (snoc G T) S t1 t2 -> step (lam t1) (lam t2)
-| step_appl : forall T S (t1 t2 : tm G (arrow T S)) t3, step t1 t2 -> step (app t1 t3) (app t2 t3)
-| step_appr : forall T S (t1 : tm G (arrow T S)) (t2 t3 : tm G T), step t2 t3 -> step (app t1 t2) (app t1 t3)
-| step_pairl : forall T S (t1 t1' : tm G T) (t2 : tm G S), step t1 t1' -> step (pair t1 t2) (pair t1' t2)
-| step_pairr : forall T S (t1 : tm G T) (t2 t2' : tm G S), step t2 t2' -> step (pair t1 t2) (pair t1 t2')
-| step_fst : forall T S (t t' : tm G (times T S)), step t t' -> step (fst t) (fst t')
-| step_snd : forall T S (t t' : tm G (times T S)), step t t' -> step (snd t) (snd t')
-| step_inl : forall T S (t t' : tm G T), step t t' -> step (inl S t) (inl S t')
-| step_inr : forall T S (t t' : tm G S), step t t' -> step (inr T t) (inr T t')
-| step_case : forall T S C (t t' : tm G (plus T S)) (t1 : tm (snoc G T) C) t2, step t t' -> step (case t t1 t2) (case t' t1 t2)
-| step_case1 : forall T S C (t : tm G (plus T S)) (t1 t1' : tm (snoc G T) C) t2, @step _ _ t1 t1' -> step (case t t1 t2) (case t t1' t2)
-| step_case2 : forall T S C (t : tm G (plus T S)) (t1 : tm (snoc G T) C) t2 t2', @step _ _ t2 t2' -> step (case t t1 t2) (case t t1 t2')
-| step_inj : forall F (t t' : tm G (app_fsub1 F (mu F))), step t t' -> step (inj F t) (inj F t')
-| step_rec1 : forall F C (t1 t1' : tm G (mu F)) (t2 : tm (snoc nil (app_fsub1 F C)) C), step t1 t1' -> step (rec t1 t2) (rec t1' t2)
-| step_rec2 : forall F C (t1 : tm G (mu F)) (t2 t2' : tm (snoc nil (app_fsub1 F C)) C), @step _ _ t2 t2' -> step (rec t1 t2) (rec t1 t2')
-| step_out : forall F (t t' : tm G (nu F)), step t t' -> step (out t) (out t')
-| step_corec1 : forall F C (t1 t1' : tm G C) (t2 : tm (snoc nil C) (app_fsub1 F C)), step t1 t1' -> step (corec F t1 t2) (corec F t1' t2)
-| step_corec2 : forall F C (t1 : tm G C) (t2 t2' : tm (snoc nil C) (app_fsub1 F C)), @step _ _ t2 t2' -> step (corec F t1 t2) (corec F t1 t2')
+| step_lam : forall T S (t1 t2 : tm (snoc G T) S), @step (snoc G T) S t1 t2 -> step (tlam t1) (tlam t2)
+| step_appl : forall T S (t1 t2 : tm G (arrow T S)) t3, step t1 t2 -> step (tapp t1 t3) (tapp t2 t3)
+| step_appr : forall T S (t1 : tm G (arrow T S)) (t2 t3 : tm G T), step t2 t3 -> step (tapp t1 t2) (tapp t1 t3)
+| step_pairl : forall T S (t1 t1' : tm G T) (t2 : tm G S), step t1 t1' -> step (tpair t1 t2) (tpair t1' t2)
+| step_pairr : forall T S (t1 : tm G T) (t2 t2' : tm G S), step t2 t2' -> step (tpair t1 t2) (tpair t1 t2')
+| step_fst : forall T S (t t' : tm G (times T S)), step t t' -> step (tfst t) (tfst t')
+| step_snd : forall T S (t t' : tm G (times T S)), step t t' -> step (tsnd t) (tsnd t')
+| step_inl : forall T S (t t' : tm G T), step t t' -> step (tinl S t) (tinl S t')
+| step_inr : forall T S (t t' : tm G S), step t t' -> step (tinr T t) (tinr T t')
+| step_case : forall T S C (t t' : tm G (plus T S)) (t1 : tm (snoc G T) C) t2, step t t' -> step (tcase t t1 t2) (tcase t' t1 t2)
+| step_case1 : forall T S C (t : tm G (plus T S)) (t1 t1' : tm (snoc G T) C) t2, @step _ _ t1 t1' -> step (tcase t t1 t2) (tcase t t1' t2)
+| step_case2 : forall T S C (t : tm G (plus T S)) (t1 : tm (snoc G T) C) t2 t2', @step _ _ t2 t2' -> step (tcase t t1 t2) (tcase t t1 t2')
+| step_inj : forall F (t t' : tm G (app_fsub1 F (mu F))), step t t' -> step (tinj F t) (tinj F t')
+| step_rec1 : forall F C (t1 t1' : tm G (mu F)) (t2 : tm (snoc nil (app_fsub1 F C)) C), step t1 t1' -> step (trec t1 t2) (trec t1' t2)
+| step_rec2 : forall F C (t1 : tm G (mu F)) (t2 t2' : tm (snoc nil (app_fsub1 F C)) C), @step _ _ t2 t2' -> step (trec t1 t2) (trec t1 t2')
+| step_out : forall F (t t' : tm G (nu F)), step t t' -> step (tout t) (tout t')
+| step_corec1 : forall F C (t1 t1' : tm G C) (t2 : tm (snoc nil C) (app_fsub1 F C)), step t1 t1' -> step (tcorec F t1 t2) (tcorec F t1' t2)
+| step_corec2 : forall F C (t1 : tm G C) (t2 t2' : tm (snoc nil C) (app_fsub1 F C)), @step _ _ t2 t2' -> step (tcorec F t1 t2) (tcorec F t1 t2')
 
+| step_arrow : forall T S (t1 : tm (snoc G T) S) (t2 : tm G T), step (tapp (tlam t1) t2) (app_tsub1 t1 t2)
 .
