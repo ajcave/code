49195b9c20c2b8bb0a3156ed2cc1288b46a6e659
Andrew Cave <ajcave@coqbox.(none)>
2013-02-11 20:21:12 -0500

    blah

--- mu-calculus/strong_norm_coq/sn.v
+++ mu-calculus/strong_norm_coq/sn.v
@@ -50,7 +50,7 @@ Inductive sort : Type :=
  | type : sort.
 
 Inductive functor (D : ctx sort) : Set :=
- | fv : var D type -> functor D
+ | fv : forall T, var D T -> functor D
  | arrow : functor nil -> functor D -> functor D
  | times : functor D -> functor D -> functor D
  | plus : functor D -> functor D -> functor D
@@ -59,7 +59,7 @@ Inductive functor (D : ctx sort) : Set :=
 
 Fixpoint app_vsub D D' (F : functor D) (s : vsub D D') : functor D' :=
 match F with
-| fv x => fv (glookup (var _) x s)
+| fv _ x => fv (glookup (var _) x s)
 | arrow A F' => arrow A (app_vsub D' F' s)
 | times F G => times (app_vsub D' F s) (app_vsub D' G s)
 | plus F G => plus (app_vsub D' F s) (app_vsub D' G s)
@@ -69,7 +69,32 @@ end.
 
 Definition fsub (D D' : ctx sort) := gsub D (fun _ => functor D').
 
+Definition wknfsub (G G' : ctx sort) T (s : fsub G G') : fsub G (snoc G' T) :=
+gmap G (fun _ => functor G') (fun _ => functor (snoc G' T)) (fun _ F => app_vsub _ F (weakening_vsub G' T)) s.
 
+Definition extfsub (G G' : ctx sort) T (s : fsub G G') : fsub (snoc G T) (snoc G' T) :=
+pair (wknfsub G G' T s) (fv (@top sort G' T)).
+
+Fixpoint idfsub (G : ctx sort) : fsub G G :=
+match G return fsub G G with
+| nil => tt
+| snoc G' T => extfsub G' G' T (idfsub G')
+end.
+
+Fixpoint app_fsub D D' (F : functor D) (s : fsub D D') : functor D' :=
+match F with
+| fv _ x => glookup _ x s
+| arrow A F' => arrow A (app_fsub D' F' s)
+| times F G => times (app_fsub D' F s) (app_fsub D' G s)
+| plus F G => plus (app_fsub D' F s) (app_fsub D' G s)
+| mu F => mu (app_fsub _ F (extfsub D D' type s))
+| nu F => nu (app_fsub _ F (extfsub D D' type s))
+end.
+
+Definition single_fsub D T F : fsub (snoc D T) D := pair (idfsub D) F.
+
+Definition app_fsub1 D T (F : functor (snoc D T)) (G : functor D) : functor D :=
+app_fsub D F (single_fsub T G).
 
 Definition tp := functor nil.
 
@@ -83,5 +108,8 @@ Inductive tm (G : ctx tp) : tp -> Set :=
  | inl : forall S T, tm G T -> tm G (plus T S)
  | inr : forall T S, tm G S -> tm G (plus T S)
  | case : forall T S C, tm G (plus T S) -> tm (snoc G T) C -> tm (snoc G S) C -> tm G C
- | 
+ | inj : forall F, tm G (app_fsub1 F (mu F)) -> tm G (mu F)
+ | rec : forall F C, tm G (mu F) -> tm (snoc nil (app_fsub1 F C)) C -> tm G C
+ | out : forall F, tm G (nu F) -> tm G (app_fsub1 F (nu F))
+ | corec : forall F C, tm G C -> tm (snoc nil C) (app_fsub1 F C) -> tm G (nu F)
 .
