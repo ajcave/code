d5911345df0c8d8dbb56b08884d436915b2213b4
Andrew Cave <acave1@cs.mcgill.ca>
2015-03-12 15:57:59 -0400

    blah

--- implementation/dtt/Typecheck.ml
+++ implementation/dtt/Typecheck.ml
@@ -3,7 +3,13 @@ module V = Value
 
 exception IllTyped
 
-let equal vtp vtp' = [] (* TODO *)
+let rec equal = function
+  | V.ConApp (id1,sp1), V.ConApp (id2,sp2) when id1 = id2 -> equalSp (sp1,sp2)
+  | V.Type , V.Type -> []
+  | v1 , v2 -> raise V.NotImplemented
+and equalSp = function
+  | V.Emp, V.Emp -> []
+  | V.Snoc (sp1, v1), V.Snoc (sp2, v2) -> (equalSp (sp1,sp2)) @ (equal (v1,v2))
 
 let solvable gamma eqns = () (* TODO *)
 
@@ -15,7 +21,7 @@ let rec chk sigma gamma =
   | (App (id, sp), a) ->
     let vtp = synthIdType id in
     let vtpr = chkSp (sp, vtp) in
-    let eqns = equal vtpr a in
+    let eqns = equal (vtpr, a) in
     solvable gamma eqns
   | (Id id, a) -> chk' (App (id, []), a)
   | (Type, V.Type) -> ()
@@ -35,10 +41,10 @@ let rec chkPat sigma (p,tp) = match p with
   | App (ident,ps) ->
     let V.Constr vtp = V.lookuptp sigma ident in 
     let (gamma1, vtp') = chkPats sigma (ps, vtp) in
-    let eqns = equal vtp' tp in
+    let eqns = equal (vtp', tp) in
     (eqns@gamma1)
   | Id ident ->
-    try let V.Constr vtp = V.lookuptp sigma ident in equal vtp tp
+    try let V.Constr vtp = V.lookuptp sigma ident in equal (vtp, tp)
     with V.Free -> [(ident,tp)] (* It's a variable *)
 
 and chkPat1 sigma p a f =
@@ -69,7 +75,7 @@ let rec chkDecl sigma = function
     let d = (name,V.Constr V.Type) in
      d::(List.map (fun (Con (cname, ctp)) ->
        chk (d::sigma) [] (ctp, V.Type);
-       (cname,(V.Constr (V.eval sigma ctp)))) constructors)
+       (cname,(V.Constr (V.eval (d::sigma) ctp)))) constructors)
 
 let rec chkDeclList sigma defs = match defs with
   | [] -> ()
